794703badc960a71d992a3f3bbe640e2a467a171
yeli88 <ye.li@pershing.com>
2018-12-18 18:17:06 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
    
 
   let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+  scanner.addListener('scan', function (content) {
+    var data = content.split(":");
+
+    var name = data[4].slice(0, -3);
+    var id = data [9].slice(0,-7);
+
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("id").innerHTML = id;
+  });
   
   Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
@@ -14,7 +23,8 @@
 
   function scan(){
   let result = scanner.scan();
-  document.getElementById("content").innerHTML = result.content;
+  //document.getElementById("content").innerHTML = result.content;
+  document.getElementById("content").innerHTML = "Success";
 };
 // Start the video stream when the window loads
 //window.addEventListener("load", cameraStart, false);
